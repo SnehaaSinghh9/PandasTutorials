a220ac65a4f498f8e5162119b59f83e0718b79da
SnehaaSinghh9 <101444040+SnehaaSinghh9@users.noreply.github.com>
2024-01-16 22:13:29 +0530

    Pandas Installation and Version

---
+++ p1_installation.py
@@ -0,0 +1,7 @@
+# Video1:- Pandas is used for cleaning, analyzing and processing the data sets.
+#1.Install pandas - pip install pandas
+#2.import pandas
+#3.check pandas version
+
+import pandas as pd 
+print(pd.__version__)
